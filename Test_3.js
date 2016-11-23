ba46d21cbc9d6b3ba5bbe93f456dddfeb0c59eaf
ccaven <chacecaven@outlook.com>
2016-11-23 16:10:16 -0500

    Update Test_3.js

--- Test_3.js
+++ Test_3.js
@@ -1,10 +1,14 @@
 var QRAR = require('qrar');
 var drone = require('ar-drone');
 var client  = drone.createClient();
-
+var t = 0;
 client.animateLeds('blinkRed', 5, 3);
 client.takeoff();
-client.stop();
-client.animate('yawDance',1000);
-client.stop();
+while(t<30){
+  if(t===15){
+    client.animate('yawDance',1000);
+  }
+  client.stop();
+  t++;
+}
 client.land();
