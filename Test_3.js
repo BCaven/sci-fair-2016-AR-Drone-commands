2d9b2d7ab0d35d5d1de424f967e13d75c4ff286f
BCaven <blakecaven@outlook.com>
2016-11-25 19:44:40 -0500

    Update Test_3.js

--- Test_3.js
+++ Test_3.js
@@ -2,7 +2,7 @@
 var QRAR = require('qrar');
 var drone = require('ar-drone');
 var client  = drone.createClient();
-
+client.ftrim();
 client.animateLeds('blinkRed', 5, 3);
 client.takeoff();
 client.stop();
