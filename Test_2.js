f7cf74985a255df0085b34f5ddc8541b3bd97a2c
BCaven <blakecaven@outlook.com>
2016-11-25 19:27:48 -0500

    Update Test_2.js
    Changed comments

--- Test_2.js
+++ Test_2.js
@@ -1,7 +1,7 @@
 var QRAR = require('qrar');
 var drone = require('ar-drone');
 var client  = drone.createClient();
-"""Makes drone takeoff, stabalize and land"""
+//Makes drone takeoff, stabalize and land
 client.animateLeds('blinkRed', 5, 3);
 client.takeoff();
 client.stop();
