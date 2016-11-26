4ffc8c5c6a7e1f6d8e90eff9141899e84b00efe7
BCaven <blakecaven@outlook.com>
2016-11-25 19:47:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var QRAR = require('qrar');
 var drone = require('ar-drone');
 var client  = drone.createClient();
-
+this.stop();
 client.animateLeds('blinkRed', 5, 1);
 var codes = new QRAR(drone);
 
 codes.on('qrcode', function (code) {
   console.log(code);
+  client.takeoff();
+  client.stop();
   client.animateLeds('blinkOrange', 5, 1);
+  client.land();
 
 });
 
