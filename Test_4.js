f4831756559981eecefadebb9f620e8a7267d682
BCaven <blakecaven@outlook.com>
2016-12-04 14:38:57 -0500

    Update Test_4.js

--- Test_4.js
+++ Test_4.js
@@ -8,25 +8,25 @@ client.animateLeds('blinkRed', 5, 4);
 client.ftrim();
 client.takeoff(client.calibrate(0));
 client.land();
-//var codes = new QRAR(drone);
-//client.front(0.5);
-//client.after(2500, function() {
-//    this.stop();
-//    this.down(0.1);
-//  })
-//  .after(3000, function() {
-//    this.stop();
-//    codes.on('qrcode', function (code) {
-//      console.log();
-//      client.animateLeds('blinkRed',5,1);
+var codes = new QRAR(drone);
+client.front(0.5);
+client.after(2500, function() {
+    this.stop();
+    this.down(0.1);
+  })
+  .after(3000, function() {
+    this.stop();
+    codes.on('qrcode', function (code) {
+      console.log();
+      client.animateLeds('blinkRed',5,1);
       //client.on('navdata', console.log, 10);
-//      client.land();
-//    });
-//    codes.start();
-//  })
-//  .after(10000, function(){
-//    this.land();
-//  })
+      client.land();
+    });
+    codes.start();
+  })
+  .after(10000, function(){
+    this.land();
+  })
 //codes.on('qrcode', function (code) {
 //  console.log();
 //  client.animateLeds('blinkRed',5,1);
