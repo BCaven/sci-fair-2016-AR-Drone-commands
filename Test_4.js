cc3d1c0810a73d7abd3959fdd6088ebcc0081739
BCaven <blakecaven@outlook.com>
2017-01-07 14:54:49 -0500

    Update Test_4.js

--- Test_4.js
+++ Test_4.js
@@ -3,7 +3,6 @@ var drone = require('ar-drone');
 var client  = drone.createClient();
 //change camera to downwards facing
 client.config('video:video_channel', 3);
-
 client.animateLeds('blinkRed', 5, 4);
 client.ftrim();
 client.takeoff(client.calibrate(0));
@@ -19,19 +18,10 @@ client.after(2500, function() {
     codes.on('qrcode', function (code) {
       console.log();
       client.animateLeds('blinkRed',5,1);
-      //client.on('navdata', console.log, 10);
-//      client.land();
     });
     codes.start();
   })
   .after(10000, function(){
     this.land();
   })
-//codes.on('qrcode', function (code) {
-//console.log();
-//  client.animateLeds('blinkRed',5,1);
-//  client.on('navdata', console.log, 10);
-//  client.land();
 });
-
-//codes.start();
