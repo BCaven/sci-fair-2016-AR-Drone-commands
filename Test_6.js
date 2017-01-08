aef061e0915e01f8b41b3febc60a4633ce87ea81
BCaven <blakecaven@outlook.com>
2017-01-08 12:31:21 -0500

    Update Test_6.js

--- Test_6.js
+++ Test_6.js
@@ -3,19 +3,13 @@ var drone = require('ar-drone');
 var client  = drone.createClient();
 client.setMaxListeners(10);
 client.ftrim();
-//client.animateLeds('blinkRed', 5, 3);
+client.animateLeds('blinkRed', 5, 3);
 client.takeoff();
 client.config('video:video_channel', 3);
 var codes = new QRAR(drone);
-
+client.front(0.1);
 codes.on('qrcode', function (code) {
   console.log(code);
-  if (code = 'client.land();'){
-    client.animateLeds('blinkGreen',5,1);
-  }
-  if (code = 'client.takeoff();'){
-    client.animateLeds('blinkRed', 5, 1);
-  }
 });
 
 codes.start();
