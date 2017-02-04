968302f1dfc547ed4ccc985316e0d7ca77c7cc6d
BCaven <blakecaven@outlook.com>
2017-02-04 15:24:17 -0500

    Update Test_12.js

--- Test_12.js
+++ Test_12.js
@@ -1,28 +1,18 @@
 var autonomy = require('ardrone-autonomy');
 var drone = require('ar-drone');
 var QRAR = require('qrar');
-
 var bccodes = new QRAR(drone);
 var mission  = autonomy.createMission();
 bccodes.on('qrcode', function (code) {
-  mission.client().animateLeds('fire', 5, 1);
-  mission.wait(500);
-//  console.log(code);
-  mission.client().animate('yawDance',1000);
-  mission.go({x: 0, y: 0, z: 1});
-  mission.client().animateLeds('fire', 5, 1);
+  console.log(code);
   mission.client().land();
 });
-mission.client().animateLeds('blinkRed',5,3);
 mission.client().ftrim();
-mission.client().animateLeds('blinkRed',5,3);
 mission.client().config('video:video_channel', 3);
-mission.client().animateLeds('blinkRed',5,3);
 mission.takeoff()
       mission.zero()
       mission.go({x: 0, y: 0, z: 1})
       mission.go({x: 2, y: 0, z: 0.6})
-      mission.client().animateLeds('blinkRed',5,3)
       mission.client().config('video:video_channel', 3)
       bccodes.start()
 
